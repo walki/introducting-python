139fa1a6cba087603f7c5be3d2723177091bd011
Roger Walker <roger.john.walker@gmail.com>
2022-05-09 23:39:24 -0400

    chap 1 examples

---
+++ chap1.py
@@ -0,0 +1,22 @@
+for countdown in 5,4,3,2,1, "hey":
+    print(countdown)
+
+spells = [
+    "Riddikulus!",
+    "Wingardium Levisoa!",
+    "Avada Kedavra!",
+    "Expecto Patronum!",
+    "Nox!",
+    "Lumos!"
+]
+
+print(spells[3]);
+
+quotes = {
+    "Moe": "A wise guy, huh?",
+    "Larry": "Ow!",
+    "Curly": "Nyuk nyuk!",
+}
+
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
